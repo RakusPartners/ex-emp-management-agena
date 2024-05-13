69e8f0e84d1d03e1e0d43718d815f3587476c4e2
AGE07 <kyu.yo.007@gmail.com>
2024-05-13 17:25:19 +0900

    change mvn

---
+++ src/main/java/com/example/ExEmpManegmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.exempmanegment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExEmpManegmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExEmpManegmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExEmpManegmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exempmanegment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExEmpManegmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
